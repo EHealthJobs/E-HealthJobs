7dc1b87f5056f88ee3656f51ab464b92450df8ef
pankaj dadhich <pankaj.d@ibirdsservices.com>
2025-10-25 19:41:32 +0530

    new leasted code for icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import Providers from "./providers";
 
 export const metadata = {
   title: "eHealthJOBS - Global Healthcare Recruitment Solutions",
+  icons: {
+    icon: "https://static.wixstatic.com/media/790a51_606f3f9208aa404bbf8268164deecdb3~mv2.png/v1/fill/w_670,h_274,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/790a51_606f3f9208aa404bbf8268164deecdb3~mv2.png",
+  },
   description:
     "Transform your healthcare staffing with zero travelers. Save millions with our global recruitment solutions for nurses and allied health professionals.",
 };
